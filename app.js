63fcca686278d1dbcf80fb4ea37d3275e50774eb
Debazar Yinpang <yinpangdebazar@gmail.com>
2024-10-29 00:38:27 +0000

    fixed errors on connection with database

--- app.js
+++ app.js
@@ -2,18 +2,35 @@ require("dotenv").config();
 const express = require("express");
 const expressLayout = require("express-ejs-layouts");
 const connectDB = require("./server/config/db");
-
+//const { flash } = require("express-flash-message");
+const session = require("express-session");
 const app = express();
+const flash = require("express-flash");
 const port = process.env.PORT || 5001;
 
 //Database
 connectDB();
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Static files
 app.use(express.static("public"));
+
+//Express Session
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7, //1 week
+    },
+  })
+);
+
+//flash Messages
+app.use(flash({ sessionKeyName: "flashMessage" }));
 
 // Templating Engine
 app.use(expressLayout);
--- server/controllers/customerController.js
+++ server/controllers/customerController.js
@@ -1,3 +1,6 @@
+const mongoose = require("mongoose");
+const Customer = require("../model/Customer");
+
 exports.homepage = async (req, res) => {
   const locals = {
     title: "Nodejs",
@@ -12,4 +15,24 @@ exports.addCustomer = async (req, res) => {
     description: "Nodejs free management system app",
   };
   res.render("customer/add", locals);
+};
+
+exports.postCustomer = async (req, res) => {
+  console.log("Request body:", req.body); // Debugging line
+
+  try {
+    // Pass `req.body` directly
+    const newCustomer = await Customer.create({
+      firstname: req.body.firstName,
+      lastname: req.body.lastName,
+      tel: req.body.tel,
+      email: req.body.email,
+      details: req.body.details, // Add details if in schema
+    });
+
+    res.redirect("/");
+  } catch (error) {
+    console.error("Error:", error.message);
+    res.status(500).send("An error occurred while saving the customer.");
+  }
 };
