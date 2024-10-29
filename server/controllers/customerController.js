59468a6b8152b9cf079bd31313cf67dd2e397a7b
Debazar Yinpang <yinpangdebazar@gmail.com>
2024-10-29 13:50:24 +0000

    Added pagination

--- server/controllers/customerController.js
+++ server/controllers/customerController.js
@@ -2,11 +2,34 @@ const mongoose = require("mongoose");
 const Customer = require("../model/Customer");
 
 exports.homepage = async (req, res) => {
+  const messages = await req.flash("info");
   const locals = {
     title: "Nodejs",
     description: "Nodejs free management system app",
   };
-  res.render("index", locals);
+
+  let perPage = 12;
+  let page = req.query.page || 1;
+
+  try {
+    const customers = await Customer.aggregate([{ $sort: { createdAt: -1 } }])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+    // Count is deprecated. Use countDocuments({}) or estimatedDocumentCount()
+    // const count = await Customer.count();
+    const count = await Customer.countDocuments({});
+
+    res.render("index", {
+      locals,
+      customers,
+      current: page,
+      pages: Math.ceil(count / perPage),
+      messages,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 exports.addCustomer = async (req, res) => {
@@ -29,6 +52,7 @@ exports.postCustomer = async (req, res) => {
       email: req.body.email,
       details: req.body.details, // Add details if in schema
     });
+    await req.flash("info", "new customer has been added.");
 
     res.redirect("/");
   } catch (error) {
